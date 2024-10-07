0187e03fde8479c16a6783a87bcb85ccf7972a33
Anand Prakash Yadav <168480243+anandprakashyadav735@users.noreply.github.com>
2024-10-08 00:54:56 +0530

    insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
@@ -58,7 +58,7 @@ function Insertion()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
