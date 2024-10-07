0b6a87e3a00fd1c40a939e853a87975735c6ce17
Anand Prakash Yadav <168480243+anandprakashyadav735@users.noreply.github.com>
2024-10-08 00:55:29 +0530

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
@@ -94,7 +94,7 @@ function runalgo()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
