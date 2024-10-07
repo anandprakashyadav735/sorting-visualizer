92d8aaf8232d918ac7545b2fedb37f14192665e6
Anand Prakash Yadav <168480243+anandprakashyadav735@users.noreply.github.com>
2024-10-08 00:57:18 +0530

    visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
@@ -59,7 +59,7 @@ function enable_buttons()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   ANAND PRAKASH YADAV
     
     *****************
 */
